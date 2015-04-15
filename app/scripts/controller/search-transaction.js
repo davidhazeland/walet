c11a76d63a9c76ba0e3beb6c90de92610f572f47
davidhazeland <davidhazeland@gmail.com>
2015-04-15 14:03:10 +0700

    fix search controller

--- app/scripts/controller/search-transaction.js
+++ app/scripts/controller/search-transaction.js
@@ -2,7 +2,7 @@
  * @Author: ThanhCong
  * @Date:   2015-04-08 16:35:18
  * @Last Modified by:   ThanhCong
- * @Last Modified time: 2015-04-15 13:16:24
+ * @Last Modified time: 2015-04-15 14:02:53
  */
 
 'use strict';
@@ -11,13 +11,21 @@
 
 define(['app', 'commandBus', 'observer'], function(app, CommandBus, Observer) {
 	var Controller = function($scope, $element) {
+		var input = $element.find('input');
+		
+		$scope.isTyping = false;
 		$scope.handleSearchBoxKeyUp = function(e) {
+			var term = input.val();
 			if (e.keyCode == 13) {
-				var term = $element.find('input').val();
 				CommandBus.execute('SearchTransaction', {
 					term: term
 				});
 			}
+			$scope.isTyping = (term == '') ? false : true;
+		}
+		$scope.handleCancelBtnClick = function(){
+			input.val('');
+			$scope.isTyping = false;
 		}
 	};
 
